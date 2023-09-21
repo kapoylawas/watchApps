306d2d53a045f5b5da06e9df77f2b326f70fbae1
arief sangga <arif.sangga@gmail.com>
2023-09-21 11:14:19 +0700

    update menghilangkan tombol close di ganti di klik layar

--- src/screens/info/Index.jsx
+++ src/screens/info/Index.jsx
@@ -9,59 +9,47 @@ export default function InfoScreen() {
   const navigation = useNavigation();
   return (
     <View style={{flex: 1, backgroundColor: '#3C3C3C'}}>
-      <View
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-          marginTop: 10,
-        }}>
-        <TouchableOpacity onPress={() => navigation.navigate('HomeScreen')}>
-          <MaterialIcons
-            name="close"
-            style={{
-              justifyContent: 'center',
-              alignItems: 'center',
-              width: 30,
-              height: 30,
-              borderRadius: 45,
-              padding: -1,
-            }}
-            size={30}
-            color="#fff"
-          />
-        </TouchableOpacity>
-      </View>
-      <View
-        style={{
-          justifyContent: 'center',
-          alignItems: 'center',
-          marginTop: 90,
-        }}>
-        <View style={styles.card}>
-          <View
-            style={{
-              justifyContent: 'center',
-              alignItems: 'center',
-              marginBottom: 'auto',
-            }}>
-            <Image
-              source={require('../../assets/icons/logojam.png')}
-              style={{width: 100, height: 100}}
-            />
-            <Text style={{fontSize: 18, fontWeight: 'bold', color: '#D39001'}}>
-              WATCHES
-            </Text>
-            <Text style={{fontWeight: 'bold'}}>TRADER</Text>
-            <Text style={styles.text}>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
-            </Text>
+      <TouchableOpacity onPress={() => navigation.navigate('HomeScreen')}>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            marginTop: 10,
+          }}
+        />
+        <View
+          style={{
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginTop: 90,
+          }}>
+          <View style={styles.card}>
+            <View
+              style={{
+                justifyContent: 'center',
+                alignItems: 'center',
+                marginBottom: 'auto',
+              }}>
+              <Image
+                source={require('../../assets/icons/logojam.png')}
+                style={{width: 100, height: 100}}
+              />
+              <Text
+                style={{fontSize: 18, fontWeight: 'bold', color: '#D39001'}}>
+                WATCHES
+              </Text>
+              <Text style={{fontWeight: 'bold'}}>TRADER</Text>
+              <Text style={styles.text}>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book.
+              </Text>
+            </View>
           </View>
         </View>
-      </View>
+      </TouchableOpacity>
     </View>
   );
 }
