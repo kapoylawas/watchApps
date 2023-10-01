5e513eb7b1c1a89f3b0242727aa55a1d801bed55
Arib RS <aribrhmn@gmail.com>
2023-10-01 19:28:31 +0700

    (fix) announcement screen

--- src/screens/info/Index.jsx
+++ src/screens/info/Index.jsx
@@ -10,43 +10,31 @@ export default function InfoScreen() {
   return (
     <View style={{flex: 1, backgroundColor: '#3C3C3C'}}>
       <TouchableOpacity onPress={() => navigation.navigate('HomeScreen')}>
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            marginTop: 10,
-          }}
-        />
-        <View
-          style={{
-            justifyContent: 'center',
-            alignItems: 'center',
-            marginTop: 90,
-          }}>
-          <View style={styles.card}>
-            <View
+        <View style={styles.card}>
+          <View
+            style={{
+              alignItems: 'center',
+            }}>
+            <Image
+              source={require('../../assets/watches-trader/logo/logo-white.png')}
               style={{
-                justifyContent: 'center',
-                alignItems: 'center',
-                marginBottom: 'auto',
-              }}>
-              <Image
-                source={require('../../assets/icons/logojam.png')}
-                style={{width: 100, height: 100}}
-              />
-              <Text
-                style={{fontSize: 18, fontWeight: 'bold', color: '#D39001'}}>
-                WATCHES
-              </Text>
-              <Text style={{fontWeight: 'bold'}}>TRADER</Text>
-              <Text style={styles.text}>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book.
-              </Text>
-            </View>
+                width: 70,
+                height: 70,
+                resizeMode: 'contain',
+              }}
+            />
+            <Text style={styles.text}>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              ever since the 1500s, when an unknown printer took a galley of
+            </Text>
           </View>
         </View>
       </TouchableOpacity>
@@ -56,17 +44,18 @@ export default function InfoScreen() {
 
 const styles = StyleSheet.create({
   card: {
-    width: 250,
-    height: 400,
+    width: 300,
     backgroundColor: '#483729',
-    borderRadius: 150, // Mengatur border radius untuk membuat card menjadi oval
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 10,
+    borderRadius: 150,
+    paddingHorizontal: 30,
+    paddingBottom: 100,
+    paddingTop: 50,
+    alignSelf: 'center',
+    marginVertical: 70,
   },
   text: {
-    color: '#8E6413', // Warna emas
+    marginTop: 30,
+    color: '#D39000', // Warna emas
     textAlign: 'center', // Center-align the text
-    fontSize: 15,
   },
 });
